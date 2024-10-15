c91714c7275b5bb882e85eb99005b35a49d33751
Breno Giovani <brenogpf22@hotmail.com>
2024-10-14 21:47:11 -0300

    Nova demanda criada

--- trabalho_conecta_work/lib/components/my_app_bar.dart
+++ trabalho_conecta_work/lib/components/my_app_bar.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:trabalho_conecta_work/pages/nova_demanda.dart'; // Certifique-se de que o caminho esteja correto
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyAppBar({super.key});
+  final bool showAddIcon; // Adiciona o parâmetro para controlar a exibição do ícone
+
+  // Construtor que aceita o parâmetro showAddIcon (com valor padrão true)
+  const MyAppBar({super.key, this.showAddIcon = true}); 
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false, // Remove o botão de voltar automático
       title: Image.asset(
         "assets/imagens/logo_b.png",
         height: 40, // Defina a altura da imagem
@@ -13,12 +18,20 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
       ),
       backgroundColor: const Color.fromRGBO(0, 74, 173, 1),
       elevation: 0,
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.add, color: Colors.white),
-        ),
-      ],
+      actions: showAddIcon // Condicional para mostrar ou não o ícone "+"
+          ? [
+              IconButton(
+                onPressed: () {
+                  // Navega para a página NovaDemanda quando o ícone "mais" é pressionado
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const NovaDemanda()),
+                  );
+                },
+                icon: const Icon(Icons.add, color: Colors.white),
+              ),
+            ]
+          : null, // Se showAddIcon for false, não exibe o ícone
     );
   }
 
---
+++ trabalho_conecta_work/lib/pages/nova_demanda.dart
@@ -0,0 +1,207 @@
+import 'dart:io'; 
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:trabalho_conecta_work/components/my_app_bar.dart';
+import 'package:image_picker/image_picker.dart'; 
+
+class NovaDemanda extends StatefulWidget {
+  const NovaDemanda({super.key});
+
+  @override
+  _NovaDemandaState createState() => _NovaDemandaState();
+}
+
+class _NovaDemandaState extends State<NovaDemanda> {
+  // Controladores para os campos de texto
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _valueController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  List<XFile>? _imageFiles; // Armazena as imagens selecionadas
+
+  // Títulos originais para cada campo
+  final String _titlePlaceholder = 'Selecione título da demanda';
+  final String _valuePlaceholder = 'Valor';
+  final String _locationPlaceholder = 'Localização';
+  final String _descriptionPlaceholder = 'Descrição do serviço';
+  final String _imagePlaceholder = 'Adicionar Imagens';
+
+ 
+  Widget _buildEditableField(String placeholder, TextEditingController controller,
+      {bool isNumeric = false}) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0), 
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8, 
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.grey[200], 
+          borderRadius: BorderRadius.circular(8.0), // Bordas arredondadas
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, 3), // Sombra para baixo
+            ),
+          ],
+        ),
+        child: Focus(
+          onFocusChange: (hasFocus) {
+            if (!hasFocus && controller.text.isEmpty) {
+              controller.text = ''; // Retorna ao texto original se não houver alteração
+            } else if (!hasFocus && controller.text.isNotEmpty) {
+              controller.text = controller.text; // Mantém o texto se foi alterado
+            }
+          },
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: controller.text.isEmpty ? placeholder : '', // Esconde o texto original ao editar
+              border: InputBorder.none, // Remove a borda padrão
+              prefixText: isNumeric ? 'R\$ ' : null, 
+            ),
+            keyboardType: isNumeric ? TextInputType.numberWithOptions(decimal: true) : TextInputType.text,
+            onTap: () {
+              // Limpa o campo ao tocar
+              if (controller.text == placeholder) {
+                controller.clear(); // Limpa o texto placeholder
+              }
+            },
+            onChanged: (value) {
+              // Atualiza a visibilidade do placeholder
+              if (isNumeric) {
+                // Formata o valor para real
+                value = value.replaceAll(RegExp(r'[^\d]'), ''); // Remove tudo que não for dígito
+                if (value.isNotEmpty) {
+                  double parsedValue = double.parse(value) / 100; // Converte para real
+                  controller.text = parsedValue.toStringAsFixed(2).replaceAll('.', ','); // Formata como moeda
+                  controller.selection = TextSelection.fromPosition(TextPosition(offset: controller.text.length)); // Move o cursor para o final
+                } else {
+                  controller.text = ''; // Limpa se vazio
+                }
+              } else if (value.isEmpty) {
+                controller.text = placeholder; // Se o campo estiver vazio, redefine para o placeholder
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  
+  Future<void> _selectImages() async {
+    final ImagePicker picker = ImagePicker();
+    final List<XFile>? selectedImages = await picker.pickMultiImage();
+    if (selectedImages != null) {
+      setState(() {
+        _imageFiles = selectedImages; 
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const MyAppBar(showAddIcon: false), 
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start, 
+              children: [
+                IconButton(
+                  icon: const FaIcon(FontAwesomeIcons.chevronLeft, color: Colors.black), // Ícone de voltar
+                  onPressed: () {
+                    Navigator.pop(context); // Volta para a página anterior
+                  },
+                ),
+                const SizedBox(width: 4), 
+              ],
+            ),
+            const SizedBox(height: 4), 
+            const Center( 
+              child: Text(
+                'Adicione a demanda específica',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                ),
+                textAlign: TextAlign.center, 
+              ),
+            ),
+            const SizedBox(height: 8), 
+
+            
+            _buildEditableField(_titlePlaceholder, _titleController),
+            _buildEditableField(_valuePlaceholder, _valueController, isNumeric: true), 
+            _buildEditableField(_locationPlaceholder, _locationController),
+            _buildEditableField(_descriptionPlaceholder, _descriptionController),
+
+            
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0), // Aumenta ainda mais o espaçamento abaixo do campo de imagens
+              child: GestureDetector(
+                onTap: _selectImages,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  padding: const EdgeInsets.all(12.0), // Aumenta o padding para um botão mais evidente
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200], // Cor de fundo leve
+                    borderRadius: BorderRadius.circular(8.0), // Bordas arredondadas
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3), // Sombra para baixo
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Adicionar Imagens',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Toque para adicionar várias imagens',
+                            style: TextStyle(fontSize: 12, color: Colors.grey), // Texto de instrução
+                          ),
+                        ],
+                      ),
+                      const FaIcon(FontAwesomeIcons.camera), // Ícone da câmera
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            // Exibe as imagens selecionadas
+            if (_imageFiles != null && _imageFiles!.isNotEmpty)
+              Wrap(
+                spacing: 8.0,
+                children: _imageFiles!.map((image) {
+                  return Image.file(
+                    File(image.path), // A classe File deve ser reconhecida agora
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  );
+                }).toList(),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
